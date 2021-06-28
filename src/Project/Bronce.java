261a6b1ff73ce78873db91447036614a88320555
stocaimaza <55241269+stocaimaza@users.noreply.github.com>
2021-06-28 17:56:03 -0300

    Delete src/Project directory

--- src/Project/Bronce.java
+++
@@ -1,8 +0,0 @@
-package Project;
-
-public class Bronce extends Avion{
-
-    public Bronce(String categoria, float capacidadCombustible, float costoKm, int capacidadMax, float velocidadMax, Propulsion propulsion) {
-        super(categoria, capacidadCombustible, costoKm, capacidadMax, velocidadMax, propulsion);
-    }
-}
